2c12228fd641fab140038a713ed59da11464b0c3
Pavel Koch <kouks.koch@gmail.com>
2015-11-29 17:35:26 +0100

    ještě banner

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,6 +10,7 @@
             <div class="img-header" data-image="/img/banner.png" data-width="1300" data-height="865"></div>
             <div class="img-header" data-image="/img/banner2.png" data-width="1697" data-height="1131"></div>
             <div class="img-header" data-image="/img/banner.jpg" data-width="2000" data-height="1000"></div>
+            <div class="img-header" data-image="/img/banner3.jpg" data-width="3500" data-height="1555"></div>
             -->
             <div class="img-header" data-image="/img/banner2.jpg" data-width="2000" data-height="1000"></div>
             <div class="col-12">
