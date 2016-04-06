c22b5493a988c5502b346647bd7588b582c1e8a0
Pavel Koch <kouks.koch@gmail.com>
2016-04-06 22:09:50 +0200

    banner <3

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,8 +11,9 @@
             <div class="img-header" data-image="/img/banner2.png" data-width="1697" data-height="1131"></div>
             <div class="img-header" data-image="/img/banner.jpg" data-width="2000" data-height="1000"></div>
             <div class="img-header" data-image="/img/banner2.jpg" data-width="2000" data-height="1000"></div>
-            -->
             <div class="img-header" data-image="/img/banner3.jpg" data-width="3500" data-height="1555"></div>
+            -->
+            <div class="img-header" data-image="/img/banner4.jpg" data-width="2000" data-height="1000"></div>
             <div class="col-12">
  
                 @include('partials.menu')
