4902eb82cacb6729daa7dd27afb5deb0fe1b97cf
bausano <gtasampserver@seznam.cz>
2016-04-06 09:04:44 +0200

    commit
    commit

--- app/Http/Controllers/Sites/ProductsController.php
+++ app/Http/Controllers/Sites/ProductsController.php
@@ -17,7 +17,7 @@ class ProductsController extends Controller
      */
     public function index()
     {
-        $products = App\Product::limit(10);    
+        $products = App\Product::limit(3); 
 
         return \View::make('products', [
             'title' => 'Produkty',
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -22,7 +22,7 @@
                 <h4 class="center white-text">Doporučené</h4>
             </div>
         </div>
-        <div class="col-9 grid">
+        <div class="col-9 grid" id="product_feed">
             @foreach($products->get() as $product)
                 <div class="col-4 grid-item">
                     <div class="area">
