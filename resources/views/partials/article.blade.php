64c2fd732480e6483170d2cfa87f41fef49b4973
Pavel Koch <kouks.koch@gmail.com>
2016-04-07 22:39:41 +0200

    mazání nepotřebných souborů a úprava article

--- resources/views/partials/article.blade.php
+++ resources/views/partials/article.blade.php
@@ -3,7 +3,7 @@
 	    <img src="/uploads/{{ $post->img }}" alt="">
 	    <div class="area">
 	        <h4 class="dark-grey-text left">{{ $post->name }}</h4>
-	        <p class="dark-grey-text justify"><?= $post->content ?></p>
+	        <p class="dark-grey-text justify"><?= str_limit($post->content, 200) ?></p>
 	    </div>
 	</article>
 </a>
